4f49459ec2ff93d92f45cac1d29d999e94715455
Syed Rehman <syedr37@gmail.com>
2020-07-02 01:09:03 -0500

    updated test with test_object_created_all_attributes

--- tests/unittests.py
+++ tests/unittests.py
@@ -14,6 +14,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.person.first_name, 'John')
         self.assertEqual(self.person.major, 'Software Engineering')
 
+    def test_object_created_all_attributes(self):
+        self.student = Student('Smith', 'John', 'Software Engineering', 2.1)
+
+        self.assertEqual(self.student.last_name, 'Smith')
+        self.assertEqual(self.student.first_name, 'John')
+        self.assertEqual(self.student.major, 'Software Engineering')
+        self.assertEqual(self.student.gpa, 2.1)
+
 
 if __name__ == '__main__':
     unittest.main()
